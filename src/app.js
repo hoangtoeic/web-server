7f03390488b8dc9a751e0107a2d15c9dbb9510b1
hoangtoeic <mr.hoangenglish@gmail.com>
2021-10-24 16:51:47 +0700

    web-server-test-3

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const hbs = require('hbs')
 const test = require('./utils/test')
 
 const app = express()
+const port=process.env.port||3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -110,6 +111,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port'+port)
 })
